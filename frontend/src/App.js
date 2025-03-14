e68277d50fc11613ef68514606577994fc6ada35
Danil <dglusenkov@mail.ru>
2025-03-14 13:42:29 +1000

    Исправлен импорт

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import Header from './components/Header';
 import ProductsPage from './pages/ProductsPage';
-import ProductDetailPage from './pages/';
+import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
 import ProfilePage from './pages/ProfilePage';
 
